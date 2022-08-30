74a299d31458e0ae0cc92adaca14b1e895db7afd
mrunlockedtech <jrbhrn@gmail.com>
2022-08-30 16:28:30 -0500

    Use Cached Element References and Event Listeners to create a simple todo list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("sanity check")
+//Cached Element References
+//Create Cached Element References by using ID's created in HTML
+const todoButton = document.getElementById("submit-button")
+const todoInput = document.getElementById("task")
+const todoList = document.getElementById("todo-list")
+
+//Test if elements were cached correctly
+//console.log(todoButton, todoInput, todoList)
+
+//Event Listeners
+todoButton.addEventListener('click',function(evt){
+  const li = document.createElement('li')
+  li.textContent = todoInput.value
+  console.dir(li)
+  todoInput.value = ""
+  todoList.appendChild(li)
+})
